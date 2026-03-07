6fc3f9ab997f96397a1d9290f7141ffc0a73e6cd
jdjcool1 <72653032+jdjcool1@users.noreply.github.com>
2026-03-07 16:40:58 +1000

    Update and rename count404.js to count_404.js

--- js/count_404.js
+++ js/count_404.js
@@ -1,16 +1,18 @@
 let seconds = 19;
 
+let hash = window.location.hash.substring(1);
+
+let target = hash ? hash + ".html" : "index.html";
+
 let timer = setInterval(function () {
 
     document.getElementById("count").textContent = seconds;
 
     if (seconds === 0) {
         clearInterval(timer);
-        window.location.href = "index.html";
+        window.location.href = target;
     }
 
     seconds--;
 
-
 }, 1000);
-
